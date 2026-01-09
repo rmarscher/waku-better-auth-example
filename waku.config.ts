705563b02f49261f68f390c6dada8e689ad1a5a1
Rob Marscher <rob@robmarscher.com>
2026-01-09 06:41:16 +0000

    Remove tsconfigPaths since we don't use it in the guide

--- waku.config.ts
+++
@@ -1,9 +0,0 @@
-import { fileURLToPath } from "node:url";
-import { defineConfig } from "waku/config";
-import tsconfigPaths from "vite-tsconfig-paths";
-
-export default defineConfig({
-  vite: {
-    plugins: [tsconfigPaths()],
-  },
-});
